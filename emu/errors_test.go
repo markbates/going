ea6afc02a266ee45ecc8d1e25d6583da06f01071
Mark Bates <mark@markbates.com>
2015-07-14 11:21:58 -0700

    handle all sorts of errors in emu

---
+++ emu/errors_test.go
@@ -0,0 +1,8 @@
+package emu_test
+
+// func Test_NameFromError(t *testing.T) {
+// 	r := require.New(t)
+//
+// 	r.Equal("io.EOF", io.EOF)
+// 	r.Equal("echo.HTTPError", emu.NameFromError(echo.NewHTTPError(404, "boom!")))
+// }
